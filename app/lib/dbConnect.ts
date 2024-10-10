e75c664c7d74d31bed2b3031099a87e511695a29
Vipin Kumar <vkprajapati665@gmail.com>
2024-10-10 16:59:20 +0530

    update

--- app/lib/dbConnect.ts
+++ app/lib/dbConnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";    
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/e-commerce'
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/e-commerce";
 
 export default function connectToDatabase(){
     mongoose.connect(MONGO_URI)
